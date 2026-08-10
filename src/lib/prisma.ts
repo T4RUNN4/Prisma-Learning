971fbc0c1c0be19110228712417b3e35ed690e5a
Tarunna Sen <tarunnasen@gmail.com>
2026-08-10 18:33:00 +0600

    Prisma DB URL problem solved

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if(!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not found");
+}
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
