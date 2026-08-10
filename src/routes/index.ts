96453bbafc8352f540f8d343ba215425c7abf8be
Tarunna Sen <tarunnasen@gmail.com>
2026-08-10 20:58:26 +0600

    CRUD - Products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users from "../services/user";
+import products from "../services/product"
 
 const router = Router();
 
 router.use("/", users);
+router.use("/", products);
 
 export default router;
---
+++ src/services/product.ts
@@ -0,0 +1,150 @@
+import { Router, Request, Response } from "express";
+import prisma from "../lib/prisma";
+import validateToken from "../middleware/JWTAuth";
+import adminvalidation from "../middleware/AdminValidation";
+
+const router = Router();
+
+router.post("/add-products", async (req: Request, res: Response) => {
+  const validationResult = await adminvalidation(req.headers.authorization!);
+
+  if (typeof validationResult === "string") {
+    return res.status(401).json({
+      status: "error",
+      message: validationResult,
+    });
+  }
+
+  const { title, description, stock, price } = req.body;
+  if (!title || !stock || !price) {
+    return res.status(400).json({
+      status: "error",
+      message: "Title, Stock and Price are required",
+    });
+  }
+
+  const product = await prisma.product.create({
+    data: {
+      title,
+      description,
+      stock,
+      price,
+    },
+  });
+
+  res.json({
+    status: "success",
+    message: "Product created successfully",
+    data: {
+      id: product.id,
+      title: product.title,
+      stock: product.stock,
+      price: product.price,
+    },
+  });
+});
+
+router.get("/products", async (req: Request, res: Response) => {
+  const validationResult = validateToken(req.headers.authorization!);
+
+  if (typeof validationResult === "string") {
+    return res.status(401).json({
+      status: "error",
+      message: validationResult,
+    });
+  }
+
+  const products = await prisma.product.findMany({
+    where: {
+      isDeleted: false,
+    },
+  });
+
+  res.json({
+    status: "success",
+    message: "Products fetch successfull",
+    data: products,
+  });
+});
+
+router.patch("/update-products", async (req: Request, res: Response) => {
+  const validationResult = await adminvalidation(req.headers.authorization!);
+
+  if (typeof validationResult === "string") {
+    return res.status(401).json({
+      status: "error",
+      message: validationResult,
+    });
+  }
+
+  const { id, title, description, stock, price } = req.body;
+
+  if (!id) {
+    return res.status(400).json({
+      status: "error",
+      message: "Product Id is required",
+    });
+  }
+
+  if (
+    title === undefined &&
+    description === undefined &&
+    stock === undefined &&
+    price === undefined
+  ) {
+    return res.status(400).json({
+      status: "error",
+      message: "At least one property is required",
+    });
+  }
+
+  const updatedProduct = await prisma.product.update({
+    where: { id: id },
+    data: { title, description, stock, price },
+  });
+
+  res.json({
+    status: "success",
+    message: "Product updated successfully",
+    data: {
+      id: updatedProduct.id,
+      title: updatedProduct.title,
+      description: updatedProduct.description,
+      stock: updatedProduct.stock,
+      price: updatedProduct.price,
+    },
+  });
+});
+
+router.delete("/delete-products", async (req: Request, res: Response) => {
+  const validationResult = await adminvalidation(req.headers.authorization!);
+
+  if (typeof validationResult === "string") {
+    return res.status(400).json({
+      status: "error",
+      message: validationResult,
+    });
+  }
+
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({
+      status: "error",
+      message: "Atlest one property is required",
+    });
+  }
+
+  await prisma.product.update({
+    where: { id },
+    data: {
+      isDeleted: true,
+    },
+  });
+
+  res.json({
+    status: "success",
+    message: "Product deleted successfully",
+  });
+});
+
+export default router
