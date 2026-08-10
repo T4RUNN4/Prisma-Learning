cabf7857941ea44e48fd5e02de2d342a411d08a4
Tarunna Sen <tarunnasen@gmail.com>
2026-08-10 22:05:27 +0600

    Review routes integration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+
 import users from "../services/user";
 import products from "../services/product"
+import review from "../services/review"
 
 const router = Router();
 
 router.use("/", users);
 router.use("/", products);
+router.use("/", review);
 
 export default router;
