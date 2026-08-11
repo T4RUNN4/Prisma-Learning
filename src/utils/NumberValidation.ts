9310b5d46a92b885eb1e94e729d6659c93cb661d
Tarunna Sen <tarunnasen@gmail.com>
2026-08-11 16:46:47 +0600

    Separated Number Validation Logic

---
+++ src/utils/NumberValidation.ts
@@ -0,0 +1,21 @@
+type NumberValidationResult =
+  | {
+      status: "success";
+      number: number;
+    }
+  | {
+      status: "error";
+    };
+
+const Numbervalidation = (number: unknown): NumberValidationResult => {
+  if (typeof number !== "number" || number < 0) {
+    return {
+      status: "error",
+    };
+  }
+
+  return {
+    status: "success",
+    number: number
+  }
+};
